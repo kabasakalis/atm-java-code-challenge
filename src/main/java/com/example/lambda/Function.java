8c8dda9c4280ef44f663e8ea080358112da29ba7
Spiros Kabasakalis <kabasakalis@gmail.com>
2018-05-13 01:54:00 +0300

    Added generic AtmProgram

--- src/main/java/com/example/lambda/Function.java
+++
@@ -1,8 +0,0 @@
-package com.example.lambda;
-
-/**
- * Created by kosiara on 3/28/16.
- */
-public interface Function {
-    void invoke();
-}
---
+++ src/main/java/com/kabasakalis/atm/AtmApplication.java
@@ -0,0 +1,13 @@
+package com.kabasakalis.atm;
+import com.kabasakalis.atm.AtmProgram;
+
+
+public class AtmApplication {
+  public static void main(String[] args) {
+    AtmProgram.start();
+
+    //check whether Java8 syntax compiles
+    // System.out.println("Hello world!, it works!");
+  }
+
+}
