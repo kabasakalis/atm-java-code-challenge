10f6172e608c931956ed61d2640694c98f4cde29
Spiros Kabasakalis <kabasakalis@gmail.com>
2018-05-15 03:19:09 +0300

    ls Added BanknoteBundle and getAmount method

---
+++ src/main/java/com/kabasakalis/atm/BanknoteBundle.java
@@ -0,0 +1,24 @@
+package com.kabasakalis.atm;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Optional;
+
+public class BanknoteBundle {
+  private HashMap<Banknote, BigInteger> store;
+
+  public BanknoteBundle(HashMap<Banknote, BigInteger> store) {
+
+    this.store = store;
+  }
+
+  public Optional<BigInteger> getAmount() {
+
+    return store
+        .entrySet()
+        .stream()
+        .map((a) -> a.getKey().get().multiply(a.getValue()))
+        .reduce(BigInteger::add);
+  }
+}
